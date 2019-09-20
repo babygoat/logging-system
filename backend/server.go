0708bece0804e3ccd1fae7a8a765c30a9f6f3b58
babygoat <babygoat1124@gmail.com>
2019-09-20 17:48:39 +0800

    Add logger and implement log info endpoint
    This patch adds the configuration and format for logger and implements
    on the log info endpoint.

--- backend/server.go
+++ backend/server.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"net/http"
+	"os"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
 func logErrorStackTraceHandler(c *gin.Context) {
@@ -9,12 +14,42 @@ func logErrorStackTraceHandler(c *gin.Context) {
 }
 
 func logInfoHandler(c *gin.Context) {
+	log.Info("This is log with severity of info!")
+
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
+}
+
+func init() {
+	// disable debug message
+	gin.SetMode(gin.ReleaseMode)
+
+	// setup logger configuration
+	// backend log format would be
+	// {
+	//  "timestamp": ...,
+	//  "severity": "info",
+	//  "message" : "log message or stacktrace",
+	//  "function": "main.logInfoHandler",
+	//  "file": "server.go",
+	//  }
+
+	log.SetOutput(os.Stdout)
+	log.SetFormatter(&log.JSONFormatter{
+		TimestampFormat: time.RFC3339Nano,
+		FieldMap: log.FieldMap{
+			log.FieldKeyTime:  "timestamp",
+			log.FieldKeyLevel: "severity",
+			log.FieldKeyMsg:   "message",
+			log.FieldKeyFunc:  "function",
+			log.FieldKeyFile:  "file",
+		},
+	})
+	log.SetReportCaller(true)
 
 }
 
 func main() {
 	// disable default logger(stdout/stderr)
-	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
 	router.Use(gin.Recovery())
 
