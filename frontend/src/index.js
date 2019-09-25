36ad03a255a2a8565f2f68d6a3f294e1959f78ba
babygoat <babygoat1124@gmail.com>
2019-09-25 16:03:45 +0800

    Add error log format on stackdriver
    This patch adds the error log format on stackdriver.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,19 +4,27 @@ import logger from './logger'
 const app = new Express()
 const port = '3000'
 
+const errorFunction = () => {
+  throw Error("Unknown error")
+}
+
 app.get('/info', function (req, res) {
   logger.info('This is log with severity of info!')
   res.status(200).send({'status': 'success'})
 })
 
 app.get('/errorStackTrace', function (req, res) {
-  console.error('Error stacktrace')
+  try {
+    errorFunction()
+  } catch (err) {
+    logger.error(err.stack)
+  }
   res.status(500).send({'status': 'error'})
 })
 
 app.listen(port, (err) => {
   if (err) {
-    console.error(err)
+    logger.error(err)
   }
   logger.info("success setup")
 })
